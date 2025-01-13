13e9bb80c01ada53fc3ba8c20e54ddeb4b759ce9
schoolworkpoly <antonio.titeica@polymtl.ca>
2025-01-13 12:52:50 -0500

    Exercice 1 fini

--- hello.py
+++ hello.py
@@ -1,5 +1,9 @@
 import math
 #TODO Affichez l'aire d'un cercle avec le rayon indiqué
+pi = math.pi
 rayon = 4
+aire = pi * (rayon**2)
 
 #TODO Ajoutez une ligne qui affichera votre nom et prénom à la fin
+
+print("Titeica Antonio")
